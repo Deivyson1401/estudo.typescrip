149e18187532297cc1d8ec74766ec1ef5f5f17ee
Deivyson José da Silva <139878479+Deivyson1401@users.noreply.github.com>
2023-09-26 13:47:57 +0000

    Instalando pacotes e testes iniciais

---
+++ teste.js
@@ -0,0 +1,6 @@
+var nome = "Deivyson";
+var n1 = 10;
+var n2 = 15;
+var soma = n1 + n2;
+console.log(nome);
+console.log(soma);
---
+++ teste.ts
@@ -0,0 +1,10 @@
+let nome: string = "Deivyson"
+let n1: number = 10
+let n2: number = 15
+let soma: number = n1+n2
+
+console.log(nome);
+console.log(soma);
+
+
+
